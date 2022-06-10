bfdaac968ed5edcdafc2f41acd0d4da7b9f18c5f
Nick Brian <nbjm1234@outlook.com>
2022-06-09 20:45:34 -0500

    primera parte terminada

---
+++ src/Routes/RoutesList.js
@@ -0,0 +1,14 @@
+import { Header } from "../Pages/Main/header";
+
+
+const routeList = [
+  {
+  path:'/',
+  title:'menu',
+  description: 'menu inicial',
+  element: <Header path= '/'/>
+},
+];
+
+
+export default routeList;
